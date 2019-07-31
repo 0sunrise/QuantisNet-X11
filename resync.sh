dc0779dd7e1fac50d2c4a4d0edbd396b57b03d83
0sunrise <tmiwg0@gmail.com>
2019-07-31 22:36:16 +0900

    a script for re-ssynchronization

---
+++ resync.sh
@@ -0,0 +1,19 @@
+CONFIG_FILE='quantisnet.conf'
+USER="$(whoami)"
+SRCDIR="$(pwd)"
+CONFIGFOLDER='/${USER}/.quantisnetcore'
+COIN_DAEMON='quantisnetd'
+COIN_CLI='quantisnet-cli'
+
+/usr/local/bin/$COIN_CLI stop
+cd /$CONFIGFOLDER
+rm -r blocks database debug.log peers.dat banlist.dat chainstate db.log
+wget https://github.com/0sunrise/QuantisNet-X11/raw/master/blocks.zip
+unzip blocks.zip
+rm blocks.zip
+cd
+cat << EOF >> $CONFIGFOLDER/$CONFIG_FILE
+maxconnections=16
+EOF
+
+/usr/local/bin/$COIN_DAEMON
